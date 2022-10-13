d72e13c10926be2b31581c18238fb649644bd6be
Diego Neumann <diego.neumann.drese@gmail.com>
2022-10-13 11:37:49 -0300

    Adjust save response request

--- app/Http/Controllers/MTController.php
+++ app/Http/Controllers/MTController.php
@@ -116,13 +116,13 @@ class MTController extends BaseController {
 
         curl_close($curl);
         if($err){
-            Log::error('MTController sendMT, curl to ENGINEERING', ['request'=>$request->all(), 'url'=>$url, 'headers'=>$headers, 'response'=>$response, 'headersResponse'=>$headersResponse, 'err'=>$err, 'httpCode'=>$httpCode]);
+            Log::error('MTController sendMT, curl to ENGINEERING error', ['request'=>$request->all(), 'url'=>$url, 'headers'=>$headers, 'response'=>$response, 'headersResponse'=>$headersResponse, 'err'=>$err, 'httpCode'=>$httpCode]);
             $smsMT->syn_result = Constants::CARRIER_CONNECTING_ERROR;
             $smsMT->syn_reason = 'ERROR_CALL_CARRIER';
             $smsMT->save();
             return Constants::CARRIER_CONNECTING_ERROR;
         }elseif ((int)$httpCode!=200) {
-            Log::error('MTController sendMT, curl to ENGINEERING', ['request'=>$request->all(), 'url'=>$url, 'headers'=>$headers, 'response'=>$response, 'headersResponse'=>$headersResponse, 'err'=>$err, 'httpCode'=>$httpCode]);
+            Log::error('MTController sendMT, curl to ENGINEERING error', ['request'=>$request->all(), 'url'=>$url, 'headers'=>$headers, 'response'=>$response, 'headersResponse'=>$headersResponse, 'err'=>$err, 'httpCode'=>$httpCode]);
             $smsMT->syn_result  = $headersResponse['result'];
             $smsMT->syn_reason  = 'ERROR_CALL_CARRIER';
             $smsMT->save();
