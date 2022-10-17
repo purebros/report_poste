0180ce2d1a63268ef7450d83ea9d0b1fa28a630c
Diego Neumann <diego.neumann.drese@gmail.com>
2022-10-17 10:07:27 -0300

    Change log path

--- config/logging.php
+++ config/logging.php
@@ -49,7 +49,8 @@ return [
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            //'path' => storage_path('logs/laravel.log'),
+            'path' => '/usr/local/bwms/logs/iliad-php.log',
             'level' => 'debug',
             'days' => 14,
         ],
